483ff5d6016eb23c0ab25119110d321fed793a64
hdapinnovator <hdapinnovator@gmail.com>
2025-08-26 16:16:42 +0100

    trying out the new config to git pages again

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { AuthContextProvider } from './context/AuthContext.jsx'
 import { router } from './router.jsx'
-import OnlyAdmin from './components/OnlyAdmin.jsx'
 
 
 createRoot(document.getElementById('root')).render(
